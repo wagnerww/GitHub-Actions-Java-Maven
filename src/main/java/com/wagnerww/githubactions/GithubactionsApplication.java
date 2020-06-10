de576977be9250689cce024925eb41efb5d121b6
wagnerww <wagnerricardonet@gmail.com>
2020-06-10 15:48:56 -0300

    example of pull request validation

--- src/main/java/com/wagnerww/githubactions/GithubactionsApplication.java
+++ src/main/java/com/wagnerww/githubactions/GithubactionsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubactionsApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(GithubactionsApplication.class, args);
+
 	}
 
 }
