161e2ffda4602365adb46f2ad9569e1a6cce3e3f
tmsas <tmsas@192.168.0.13>
2022-04-28 17:00:49 -0300

    Alteração de nome da aplicação heroku e adição de comentário.

--- src/main/java/com/wagnerww/githubactions/GithubactionsApplication.java
+++ src/main/java/com/wagnerww/githubactions/GithubactionsApplication.java
@@ -3,6 +3,7 @@ package com.wagnerww.githubactions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// EXEMPLO DE APLICAÇÃO SPRING
 @SpringBootApplication
 public class GithubactionsApplication {
 
