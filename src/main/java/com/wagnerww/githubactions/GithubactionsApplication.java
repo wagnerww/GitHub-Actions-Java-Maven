2093a0011e9748bc642ea5e6797fc38038e167f4
tmsasaki <tmsski@yahoo.com.br>
2022-04-28 19:49:45 -0300

    Update GithubactionsApplication.java

--- src/main/java/com/wagnerww/githubactions/GithubactionsApplication.java
+++ src/main/java/com/wagnerww/githubactions/GithubactionsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // EXEMPLO DE APLICAÇÃO SPRING
+/* Este exemplo é utilizado para ilustrar o uso do GitHub Actions */
 @SpringBootApplication
 public class GithubactionsApplication {
 
